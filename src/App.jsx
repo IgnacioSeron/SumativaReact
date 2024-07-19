e2c9ff91864ac87fc285fbf73e4c578c944545da
IgnacioSeron <ignaciogx03@gmail.com>
2024-07-19 17:59:48 -0400

    Tercer Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
+import React from 'react';
 import './components/Css.css'
 import FormularioParaNotas from './components/FormularioParaNotas';
 
 function App() {
     return(
         <div className="Main">
-    <h1 className="h1">Post It Simulator!</h1>
-        <div className="ContenedorNotas">
-            <div className="ListaNotas">
+            <h1 className="h1">Post It Simulator!</h1>
+                <div className="ContenedorNotas">
+                    <div className="ListaNotas">
 
-            </div>
+                    </div>
+                </div>
         </div>
-    </div>
     )
     
 }
--- src/components/FormularioParaNotas.jsx
+++ src/components/FormularioParaNotas.jsx
@@ -9,7 +9,7 @@ function FormularioParaNotas ({ agregarNota }) {
     const [importante, setImportante] = useState(false);
 
     //Función que Maneja Envío de Formulario más preventDefault que Evita que se Envíe Automáticamente
-    const EnvioFormulario = (e) => {
+    const handleSubmit = (e) => {
         e.preventDefault();
         //Si la Descripción no Está Vacía se Ejecita la Función agregarNota
         if(descripcion) {
@@ -20,6 +20,19 @@ function FormularioParaNotas ({ agregarNota }) {
             setImportante(false);
         }
     };
-}
+
+    //Se Hace un Retorno con un Formulario para Agregar Las Notas y Se Llama a la Función "handleSubmit" 
+    //Además de Introducir los Datos y Se Incluye "required" en la Descripción para Hacerlo Obligatorio
+    //Y Cada vez que se Cambia un Valor se Actualiza el Estado de los input y se Actualiza el Valor
+    return (
+        <form onSubmit={handleSubmit}>
+            <input className="Titulo" type="text" placeholder="Título" value={titulo} onChange={(e) => setTitulo(e.target.value)}/>
+            <input className="Descripcion" type="text" placeholder="Descripción" value={descripcion} onChange={(e) => setDescripcion(e.target.value)} required/>
+            <input className="Check" type="checkbox" checked={importante} onChange={(e) => setImportante(e.target.checked)}/>
+            <label>Importante!</label>
+            <button className="Agregar" type="submit">Agregar</button>
+        </form>
+    )
+};
 
 export default FormularioParaNotas;
